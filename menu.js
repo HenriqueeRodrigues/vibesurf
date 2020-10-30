3d2534fcdfa387e0805ccbef110ab5ae0b72fb85
HenriqueeRodrigues <henriquee.rodrigues@gmail.com>
2020-10-30 17:24:54 -0300

    header desktop

---
+++ menu.js
@@ -0,0 +1,28 @@
+const btnOpen = document.querySelector('.btn-open');
+const btnClose = document.querySelector('.btn-close');
+const closeMenu = document.querySelectorAll('[data-close="menu"]');
+const menu = document.querySelector('.menu');
+
+const body = document.querySelector('body')
+
+btnOpen.addEventListener('click', e => {
+
+  menu.classList.add('open')
+  body.classList.add('open')
+
+
+})
+
+btnClose.addEventListener('click', e => {
+
+  menu.classList.remove('open')
+  body.classList.remove('open')
+
+})
+
+closeMenu.forEach(el => {
+  el.addEventListener('click', e=> {
+      menu.classList.remove('open')
+      body.classList.remove('open');
+  })
+})
